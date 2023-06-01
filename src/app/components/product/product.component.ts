690d9e338659e88021cd6d302147121721643032
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-01 14:17:27 -0500

    Add data to component

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product.model';
 import { ApiService } from 'src/app/services/api.service';
@@ -8,7 +8,17 @@ import { ApiService } from 'src/app/services/api.service';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
   product: Product | undefined;
   constructor(private route: ActivatedRoute, private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: any) => {
+      this.apiService
+        .getProductFromCategoryById(params.productId, params.id)
+        .subscribe((data: any) => {
+          this.product = data;
+        });
+    });
+  }
 }
