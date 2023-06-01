12b32e9932117daa69ee3fe780b223232a7ab736
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-01 13:49:14 -0500

    Added route to product page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { CategoryProductsComponent } from './components/category-products/category-products.component';
+import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/categories', pathMatch: 'full' },
   { path: 'categories', component: CategoriesComponent },
   { path: 'categories/:id/products', component: CategoryProductsComponent },
+  { path: 'categories/:productId/products/:id', component: ProductComponent },
 ];
 
 @NgModule({
