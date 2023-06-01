472c3fb2ce2a389ef65c8893541a5f763ee9b738
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-01 14:07:15 -0500

    Added variable and constructor to product

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
-
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 }
