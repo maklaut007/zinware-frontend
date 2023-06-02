08b417d82dc579aa38ab3a1461bd2f3031850fe4
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-02 17:25:22 -0500

    Added path for auth components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { CategoryProductsComponent } from './components/category-products/category-products.component';
 import { ProductComponent } from './components/product/product.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/categories', pathMatch: 'full' },
   { path: 'categories', component: CategoriesComponent },
   { path: 'categories/:id/products', component: CategoryProductsComponent },
   { path: 'categories/:productId/products/:id', component: ProductComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
