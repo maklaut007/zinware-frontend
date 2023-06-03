ef856944d3890e21a27ae164e24c36c5d5c87be7
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-02 22:04:40 -0500

    Created user type variable

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
-
+import { ApiService } from 'src/app/services/api.service';
+import { User } from 'src/app/models/user.mode';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-  username: string = '';
-  email: string = '';
-  password: string = '';
+  user: User = {
+    userName: '',
+    email: '',
+    password: '',
+  };
+
+  constructor(private apiService: ApiService) {}
 
   onSubmit() {
-    console.log(this.username, this.email, this.password);
+    // this.apiService.registerUser
   }
 }
