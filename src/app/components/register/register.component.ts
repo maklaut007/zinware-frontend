5ff1cffc047b07e88b2470e6e85cafe1ee0b1d0c
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-02 22:16:16 -0500

    Handle registration in component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { User } from 'src/app/models/user.mode';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -13,9 +15,17 @@ export class RegisterComponent {
     password: '',
   };
 
-  constructor(private apiService: ApiService) {}
-
+  constructor(private apiService: ApiService, private router: Router) {}
+  /**
+   * Sends the user object to the API service to create a new user
+   * redirects to user login page if successfull
+   */
   onSubmit() {
-    // this.apiService.registerUser
+    this.apiService.registerUser(this.user).subscribe((data) => {
+      if (data) {
+        // Redirect to the "/login" page
+        this.router.navigate(['/login']);
+      }
+    });
   }
 }
