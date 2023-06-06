63fb8e24e10d1fac4104b506848461a6772c7f8b
Maksym Zinchenko <maklaut98@gmail.com>
2023-06-06 11:52:43 -0500

    Added content to checkout page

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent {
+  data: {
+    name: string;
+    address: string;
+    creditCard: string;
+    expiryDate: string;
+    cvv: string;
+  };
+  constructor() {
+    this.data = {
+      name: '',
+      address: '',
+      creditCard: '',
+      expiryDate: '',
+      cvv: '',
+    };
+  }
 
+  submitCheckout() {}
 }
